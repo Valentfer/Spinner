a0c5ddc3344f0bc9065175b5d7348c197ad89750
valen <valentin.fernandez.372@estudiante.iesoretania.es>
2022-10-11 16:59:08 +0200

    final

--- app/src/main/java/com/example/spinner/MainActivity.java
+++ app/src/main/java/com/example/spinner/MainActivity.java
@@ -3,34 +3,75 @@ package com.example.spinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.spinner.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-    private ActivityMainBinding binding;
-
+   //Declaramos las variables que vamos a usar en los métodos
+    private TextView num1, num2, txtResul;
+    private float resultado;
+    private Spinner spin;
+    String[] opciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
+        setContentView(R.layout.activity_main);
 
-        binding.spinner.setOnClickListener(view -> selecionarSpin());
+        //indicamos a las variables cuales son sus textbox
+        num1 = findViewById(R.id.txtnum1);
+        num2 = findViewById(R.id.txtnum2);
+        txtResul = findViewById(R.id.txtResul);
+
+        //instanciamos el Spinner
+        spin = findViewById(R.id.spinner);
+        spin.setOnItemSelectedListener(this);
+
+        //creamos las opciones del Spinner
+        opciones = new String[]{"", "Sumar", "Restar", "Multiplicar", "Dividir"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, opciones);
+        spin.setAdapter(adapter);
+
     }
 
 
-     public void selecionarSpin() {
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
+        //hacemos uso de switch case par las diferentes opciones que proporciona el spinner
+        String opcion = opciones[position];
+        switch(opcion) {
+            case "Sumar":
+                resultado = Integer.parseInt((num1.getText().toString())) + Integer.parseInt((num2.getText().toString()));
+                txtResul.setText(String.valueOf(resultado));
+                break;
+            case "Restar":
+                resultado = Integer.parseInt((num1.getText().toString())) - Integer.parseInt((num2.getText().toString()));
+                txtResul.setText(String.valueOf(resultado));
+                break;
+            case "Multiplicar":
+                resultado = Integer.parseInt((num1.getText().toString())) * Integer.parseInt((num2.getText().toString()));
+                txtResul.setText(String.valueOf(resultado));
+                break;
+            case "Dividir":
+                if (Integer.parseInt((num2.getText().toString())) == 0) {
+                    Toast.makeText(this, "no se puede dividir entre 0", Toast.LENGTH_SHORT).show();
+                }
+                resultado = Integer.parseInt((num1.getText().toString())) / Integer.parseInt((num2.getText().toString()));
+                txtResul.setText(String.valueOf(resultado));
+                break;
+        }
+    }
 
-         String[] opciones = {"sumar", "restar", "multiplicar", "dividir"};
+    //Estamos obligados a implementar el siguiente método aunque no se use
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
-
-
-         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, opciones);
-
-     }
-
+    }
 }
